aa5bf88cf0df3b81ff134053ddbe0c6a45b366eb
github username <github email address>
2019-03-11 14:04:06 -0700

    started scraper class

--- README.md
+++ README.md
@@ -41,3 +41,4 @@ The gem is available as open source under the terms of the [MIT License](https:/
 ## Code of Conduct
 
 Everyone interacting in the JoshuaTree project’s codebases, issue trackers, chat rooms and mailing lists is expected to follow the [code of conduct](https://github.com/<github username>/joshua_tree/blob/master/CODE_OF_CONDUCT.md).
+# joshua_tree_cli_project
--- lib/joshua_tree/scraper.rb
+++ lib/joshua_tree/scraper.rb
@@ -5,6 +5,11 @@ require 'open-uri'
 class JoshuaTree::Scraper
 
   def self.scrape
+    doc = Nokogiri::HTML(open('https://www.nps.gov/jotr/planyourvisit/campgrounds.htm'))
+    doc.css("h3.ActivityListing-title").text
+    doc.css("div.ActivityListing a").each do |campground|
+      puts campground.css("div.ActivityListing-titleContainer h3").text
+    end
     #html =
     #File.read(https://www.nps.gov/jotr/planyourvisit/campgrounds.htm)
     #webpage = Nokogiri::HTML(html)

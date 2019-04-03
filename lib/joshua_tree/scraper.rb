01473d3e3fdd20a34d56f914760bd99656226a6a
Karly Mareka <github email address>
2019-04-02 17:23:50 -0700

    removed unneeded line

--- lib/joshua_tree/scraper.rb
+++ lib/joshua_tree/scraper.rb
@@ -2,9 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 #require 'pry'
 
-class JoshuaTree::Scraper
-
-  @campgrounds = []
+class JoshuaTree::Scraper 
 
   def self.scrape
     doc = Nokogiri::HTML(open('https://www.nps.gov/jotr/planyourvisit/campgrounds.htm'))
